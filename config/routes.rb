730059b817e622f8ff06fe99da4e2bde7ebcc804
gwendal le bris <gwen.coding@gmail.com>
2023-05-24 19:02:39 +0200

    Create models Tag and PlantTag

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :gardens do
+  root "gardens#index"
+  resources :gardens, except: :index do
     resources :plants, only: :create
   end
   resources :plants, only: :destroy
@@ -8,3 +9,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+# Etapes pour ajouter le model Tag et le model Plant Tag
+# 1. Faire le schéma de DB
+# 2. Générer le model Tag
+# 3. Générer le model PlantTag
+# 4. Ajouter des associations et des validations
+# 5. Tester dans rails c
