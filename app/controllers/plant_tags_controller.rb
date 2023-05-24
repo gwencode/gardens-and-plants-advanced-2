aec22aff11a988813625fcf2eecc4aa2aa2fde47
gwendal le bris <gwen.coding@gmail.com>
2023-05-24 20:02:46 +0200

    Add Tom Select

--- app/controllers/plant_tags_controller.rb
+++ app/controllers/plant_tags_controller.rb
@@ -8,12 +8,25 @@ class PlantTagsController < ApplicationController
 
   def create
     @plant = Plant.find(params[:plant_id])
-    @tag = Tag.find(params[:plant_tag][:tag])
-    @plant_tag = PlantTag.new(plant: @plant, tag: @tag)
-    if @plant_tag.save
-      redirect_to garden_path(@plant.garden)
-    else
-      render :new, status: :unprocessable_entity
+    # For one tag
+    # @tag = Tag.find(params[:plant_tag][:tag])
+    # @plant_tag = PlantTag.new(plant: @plant, tag: @tag)
+    # if @plant_tag.save
+    #   redirect_to garden_path(@plant.garden)
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+
+    # For several tags
+    tags = Tag.where(id: params[:plant_tag][:tag])
+    tags.each do |tag|
+      plant_tag = PlantTag.new(plant: @plant, tag: tag)
+      unless plant_tag.save
+        @plant_tag = PlantTag.new
+        render :new, status: :unprocessable_entity
+        return
+      end
     end
+    redirect_to garden_path(@plant.garden)
   end
 end
